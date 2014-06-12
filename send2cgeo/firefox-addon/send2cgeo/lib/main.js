97202d738d6186d5547cc68e9964503f09bcd51d
JavaJens <javajens@web.de>
2014-06-12 22:35:48 +0200

    Add updated firefox and chrome plugin

--- send2cgeo/firefox-addon/send2cgeo/lib/main.js
+++ send2cgeo/firefox-addon/send2cgeo/lib/main.js
@@ -2,7 +2,17 @@ var pageMod = require("sdk/page-mod");
 var data = require("sdk/self").data;
  
 pageMod.PageMod({
-  include: ["http://www.geocaching.com/seek/cache_details*", "http://www.geocaching.com/geocache/*", "http://www.geocaching.com/map/*"],
+  include: ["http://www.geocaching.com/seek/cache_details*", "https://www.geocaching.com/map/*", "http://www.geocaching.com/geocache/*", "http://www.geocaching.com/my/recentlyviewedcaches*", "http://www.geocaching.com/seek/nearest*"],
   contentScriptWhen: "end",
   contentScriptFile: data.url("send2cgeo.user.js")
+},
+{
+  include: ["http://opencaching.pl/viewcache.php*", "http://www.opencaching.nl/viewcache.php*" ],
+  contentScriptWhen: "end",
+  contentScriptFile: data.url("send2cgeoOcPl.user.js")
+},
+{
+  include: ["http://www.opencaching.de/viewcache.php*"],
+  contentScriptWhen: "end",
+  contentScriptFile: data.url("send2cgeoOc.user.js")
 });
